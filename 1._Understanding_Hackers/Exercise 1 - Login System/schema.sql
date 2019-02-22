c95e34c78b6fc057a6124c50ad5f93138d00c957
anders <al@mobilize-systems.com>
2019-02-22 14:13:10 +0100

    added the insert users queries to schema

--- 1._Understanding_Hackers/Exercise 1 - Login System/schema.sql
+++ 1._Understanding_Hackers/Exercise 1 - Login System/schema.sql
@@ -9,3 +9,8 @@ CREATE TABLE `login_attempts` (
 	`user_email`	TEXT NOT NULL,
 	`timestamp`	NUMERIC NOT NULL
 );
+
+
+INSERT INTO users VALUES('anders@gmail.com', "password");
+INSERT INTO users VALUES('cosmin@gmail.com', "admin123");
+INSERT INTO users VALUES('pedro@gmail.com', "book1234");
