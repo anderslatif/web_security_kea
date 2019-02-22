1daa2460ebd0be4fba0ccdfe4d3aaac83fb641b9
anders <al@mobilize-systems.com>
2019-02-22 14:00:12 +0100

    wrote the backend logic

--- 1._Understanding_Hackers/Exercise 1 - Login System/login.php
+++ 1._Understanding_Hackers/Exercise 1 - Login System/login.php
@@ -1,5 +1,35 @@
 <?php 
     
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    if( isset($email) 
+        && isset($password) 
+        && strlen($password) >= 8 
+        && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+        try{
+            require_once './database.php';
+
+            $query = $connection->prepare('SELECT * FROM users WHERE email = :email AND password = :password');
+
+            $query->bindValue(':email', $email);
+            $query->bindValue(':password', $password);
+            $query->execute();
+
+            $users = $query->fetchAll();
+
+            $log = $connection->prepare('INSERT INTO login_attempts(user_email, timestamp, success) VALUES(:email, now, :success)');
+
+            $log->bindValue(':email', $email);
+            $log->bindValue(':success', count($users));
+            $log->execute();
+
+        } catch( PDOException $e) {
+              echo '{"status":"err","message":"cannot connect to database"}';
+              exit();  
+        }
+    }
 
 
 ?>
