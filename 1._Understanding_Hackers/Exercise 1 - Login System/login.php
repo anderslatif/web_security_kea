f39f7706b1bfe7a7beec13ae08b907588606d796
anders <al@mobilize-systems.com>
2019-03-01 09:52:25 +0100

    Merge branch 'master' of https://github.com/anderslatif/web_security_kea
    # Conflicts:
    #	1._Understanding_Hackers/Exercise 1 - Login System/signup.php

--- 1._Understanding_Hackers/Exercise 1 - Login System/login.php
+++ 1._Understanding_Hackers/Exercise 1 - Login System/login.php
@@ -19,16 +19,43 @@ if( isset($_POST["email"])
         $query->bindValue(':email', $email);
         $query->bindValue(':password', $password);
         $query->execute();
-        print "4";
-
-            $users = $query->fetchAll();
-
-            $log = $connection->prepare('INSERT INTO login_attempts(user_email, timestamp, success) VALUES(:email, DATETIME("now"), :success)');
-
-            $log->bindValue(':email', $email);
-            echo count($users);
-            $log->bindValue(':success', count($users));
-            $log->execute();
+        print "3";
+        $users = $query->fetchAll();
+        
+        $log = $connection->prepare('INSERT INTO login_attempts(user_email, timestamp, success) VALUES(:email, DATETIME("now"), :success)');
+        
+        $log->bindValue(':email', $email);
+        echo "Count of users is: ".count($users);
+        $log->bindValue(':success', count($users));
+        $log->execute();
+        
+        if (count($users) == 0) {
+            $attempts = $connection->prepare("SELECT * FROM login_attempts WHERE user_email=:email AND timestamp > date('now', '-1 hours') ORDER BY timestamp DESC LIMIT 2");
+            $attempts->bindValue(':email', $email);
+            $attempts->execute();
+            var_dump($attempts->fetchAll());
+            if user has failed twice in a row and this time, then log -1 as quarantine 
+            exit;
+        }
+        
+        
+        //     SELECT * FROM login_attempts WHERE user_email='cosmin@gmail.com' AND timestamp > date('now', '-1 hours') ORDER BY timestamp DESC LIMIT 3;
+        //     switch ($variable) {
+        //         case 'value':
+        //             # code...
+        //             break;
+                
+        //         default:
+        //             # code...
+        //             break;
+        //     }
+        //     CASE trynumber === 1 
+        //         return trynumber + 1
+        //     case tynumber === 2
+        //         return trynumber + 1
+        //     case trynumber >= 3
+        //         return 0
+        // ;
 
         } catch( PDOException $e) {
               echo '{"status":"err","message":"cannot connect to database"}';
@@ -45,28 +72,12 @@ if( isset($_POST["email"])
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
-    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-    <div class="base__division">
-    <div class="main__division">
-        <div class="main__division--forms">
-            <form method="POST" action="login.php" class="form__login">
-                <input type="text" placeholder="Email" name="email" class="email">
-                <input type="password" placeholder="Password" name="password" class="password">
-                <input type="submit" value="Submit">
-            </form>
-        </div>
-        <div class="main__division--datas">
-            <div class="datas__wrapper">
-                <h2>Login</h2>
-                <p>Web Security - Exercise</p>
-            </div>
-        </div>
-    </div>
-    </div>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="./script.js"></script>
+    <form method="POST" action="login.php" >
+        <input type="text" placeholder="Email" name="email">
+        <input type="password" placeholder="Password" name="password">
+        <input type="submit" value="Submit">
+    </form>
 </body>
 </html>
--- 1._Understanding_Hackers/Exercise 1 - Login System/signup.php
+++ 1._Understanding_Hackers/Exercise 1 - Login System/signup.php
@@ -1,16 +1,16 @@
 <?php
 
-if( isset($_POST["email"])
-&& isset($_POST["password"])
-&& strlen($_POST["password"]) >= 8
+if( isset($_POST["email"]) 
+&& isset($_POST["password"]) 
+&& strlen($_POST["password"]) >= 8 
 && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)
 && preg_match('/^[a-zA-Z0-9]+$/', $_POST["email"])) {
 
     $email = $_POST["email"];
     $password = $_POST["password"];
-
+    
     $options = [
-        'cost' => 12
+        'cost' => 12 
     ];
     $hash = password_hash($password, PASSWORD_DEFAULT, $options);
 
@@ -25,21 +25,12 @@ if( isset($_POST["email"])
         $query->execute();
 
 
-        SELECT * FROM login_attempts WHERE email='email@.something.dk' AND timestamp > date('now', '-1 hours')  DESC LIMIT 3
-            CASE trynumber === 1
-                return trynumber + 1
-            case tynumber === 2
-                return trynumber + 1
-            case trynumber >= 3
-                return
-        ;
 
         $success = $query->fetchAll();
 
     } catch( PDOException $e) {
           echo '{"status":"err","message":"cannot connect to database"}';
-          exit();
+          exit();  
     }
 }
-?>
 
