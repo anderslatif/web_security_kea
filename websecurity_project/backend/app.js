776ac87306ba9243ee746df6ab6c81e7f7d95f55
anders <al@mobilize-systems.com>
2019-04-06 22:18:37 +0200

    Added mongoose but not yet connected to the server

--- websecurity_project/backend/app.js
+++ websecurity_project/backend/app.js
@@ -7,13 +7,16 @@ app.use(morgan('combined'));
 
 const bookroute = require("./routes/bookroute");
 const userroute = require("./routes/userroute");
+app.use(bookroute);
+app.use(userroute);
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+
 
 app.get("/", (req, res) => {
    res.send("Welcome to the awesome book api. Feel right at home exploring.")
 });
-
-app.use(bookroute);
-// app.use(userroute);
 
 
 io.on('connection', socket => {
