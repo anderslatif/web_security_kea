a149e2303a3c42db665de6cb446f6ff501e99b02
anders <al@mobilize-systems.com>
2019-04-26 20:49:50 +0200

    Cleaned up in ComponentCreatePosts

--- websecurity_project/src/components/ComponentCreatePosts.js
+++ websecurity_project/src/components/ComponentCreatePosts.js
@@ -1,82 +1,88 @@
-import React, { Component } from 'react'
-import axios from "axios";
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class ComponentCreatePosts extends Component {
   constructor(props) {
-      super(props)
+      super(props);
       this.state = {
-          title: "",
-          author: "",
-          description: "",
-          cover: "",
-          file: ""
-      }
+          title: '',
+          author: '',
+          description: '',
+          cover: '',
+          file: ''
+      };
       this.testCoverUpload = this.testCoverUpload.bind(this);
       this.setPostDatas = this.setPostDatas.bind(this);
       this.uploadFile = this.uploadFile.bind(this);
       this.setTextPost = this.setTextPost.bind(this);
   }
+
+    componentDidMount() {
+        console.log('post');
+        // axios()
+        // axios.post("https://localhost:8080/post", {
+        //     title: "x"
+        // }).then(res => console.log("post some: ", res))
+        //   .catch(err => console.log("error-post request: ", err))
+    }
+
   setPostDatas = (ev) => {
-    let datas = ev.target.value;
-    let type = ev.target.name;
-    let fileDocument = ev.target.files[0];
+    const datas = ev.target.value;
+    const type = ev.target.name;
+    const fileDocument = ev.target.files[0];
 
     this.setState(() => {
-        if(type === "file") {
+        if (type === 'file') {
             return {
                 file: fileDocument
                 // [type]: datas
-            }
+            };
         }
-    })
-  }
-  setTextPost = (ev) => {
-    let dataValue = ev.target.value;
-    let dataType = ev.target.name;
+    });
+  };
 
-    this.setState({[dataType]: dataValue})
-  }
+  setTextPost = (ev) => {
+    const dataValue = ev.target.value;
+    const dataType = ev.target.name;
+
+    this.setState({ [dataType]: dataValue });
+};
+
   uploadFile = () => {
-      axios.post("http://localhost:8080/post", {
+      axios.post('http://localhost:8080/post', {
           title: this.state.title,
           description: this.state.description,
           author: this.state.author,
-          cover: "tests",
+          cover: 'tests',
           file: this.state.file
-      }).then(res => console.log("post successful: ", res))
-        .catch(error => console.log("post error: ", error))
-  }
+      }).then(res => console.log('post successful: ', res))
+        .catch(error => console.log('post error: ', error));
+  };
+
   testCoverUpload = (ev) => {
     console.log(this.ref.coverFileUploader);
-  }
-  componentDidMount() {
-    console.log("post");
-    // axios()
-    // axios.post("https://localhost:8080/post", {
-    //     title: "x"
-    // }).then(res => console.log("post some: ", res))
-    //   .catch(err => console.log("error-post request: ", err))
-  }
+  };
+
   render() {
     return (
       <div className="componentCreatePosts">
         <div className="componentCreatePosts__innernavigation">
             <button className="makeAPost" onClick={this.uploadFile}>
                 <svg>
-                    <use href="./image/sprite.svg#icon-edit"></use>
+                    <use href="./image/sprite.svg#icon-edit" />
                 </svg>
                 <span>Create Post</span>
             </button>
             <button className="addAnImage">
                 <svg onClick={this.testCoverUpload}>
-                    <use href="./image/sprite.svg#icon-image"></use>
+                    <use href="./image/sprite.svg#icon-image" />
                 </svg>
                 <span>Add Cover</span>
                 <input ref="coverFileUploader" type="file" />
             </button>
             <button className="addAFile">
                 <svg>
-                    <use href="./image/sprite.svg#icon-file-text"></use>
+                    <use href="./image/sprite.svg#icon-file-text" />
                 </svg>
                 {/* <span>Add Book file</span> */}
                 <input type="file" name="file" onChange={this.setPostDatas} />
@@ -100,7 +106,7 @@ class ComponentCreatePosts extends Component {
         {/* <div className="componentCreatePosts__innercontent"></div> */}
         {/* ComponentCreatePosts */}
       </div>
-    )
+    );
   }
 }
 
