9829f5be8512bd2c9a79599eb992ec7e59eb0a6d
anders <al@mobilize-systems.com>
2019-04-26 12:54:20 +0200

    Linted the backend

--- websecurity_project/backend/routes/postroute.js
+++ websecurity_project/backend/routes/postroute.js
@@ -1,65 +1,66 @@
-const express = require("express");
+const express = require('express');
+
 const router = express.Router();
-const Post = require("../models/Post");
-const helperFunctions = require("../helper-functions");
+const Post = require('../models/Post');
+const helperFunctions = require('../helper-functions');
 const fetch = require('node-fetch');
 
-router.get("/posts", (req, res) => {
+router.get('/posts', (req, res) => {
     if (req.session.userId) {
-        Post.find({ bookOwner: req.sesssion.userId}).exec((error, foundPosts) => {
+        Post.find({ bookOwner: req.sesssion.userId }).exec((error, foundPosts) => {
             if (error) {
-                helperFunctions.logToFile("MongoFailed" + error, "mongo-errors.txt");
+                helperFunctions.logToFile(`MongoFailed${ error}`, 'mongo-errors.txt');
             }
             if (foundPosts.length === 0) {
                 // fixme this user either looked up a user with no posts
                 // fixme or is guessing ids without knowing any
-                helperFunctions.logToFile("Someone might be trying to guess user ids to access posts", "intrusions.txt");
+                helperFunctions.logToFile('Someone might be trying to guess user ids to access posts', 'intrusions.txt');
             }
+            res.send(foundPosts);
         });
-        res.send({ post: foundPosts });
     } else {
         // fixme Someone is trying to use this route without knowing exactly what fields are required
         // fixme give them status 200 which will be considered an error status code in our client
         // fixme that will confuse them, lol
-        helperFunctions.logToFile("Someone is trying to get posts without defining an id", "intrusions.txt");
-        res.status(200).send("Signed up OK");
+        helperFunctions.logToFile('Someone is trying to get posts without defining an id', 'intrusions.txt');
+        res.status(200).send('Signed up OK');
     }
 });
 
-router.get("/posts/all", (req, res) => {
+router.get('/posts/all', (req, res) => {
     Post.find({}).exec((error, foundPosts) => {
         if (error) {
-            helperFunctions.logToFile("MongoFailed" + error, "mongo-errors.txt");
+            helperFunctions.logToFile(`MongoFailed${ error}`, 'mongo-errors.txt');
         }
         res.send(foundPosts);
     });
 });
 
 
-router.get("/posts/:userid", (req, res) => {
+router.get('/posts/:userid', (req, res) => {
     if (req.params.userid) {
-        Post.find({ bookOwner: req.params.userid}).exec((error, foundPosts) => {
+        Post.find({ bookOwner: req.params.userid }).exec((error, foundPosts) => {
             if (error) {
-                helperFunctions.logToFile("MongoFailed" + error, "mongo-errors.txt");
+                helperFunctions.logToFile(`MongoFailed${ error}`, 'mongo-errors.txt');
             }
             if (foundPosts.length === 0) {
                 // fixme this user either looked up a user with no posts
                 // fixme or is guessing ids without knowing any
-                helperFunctions.logToFile("Someone might be trying to guess user ids to access posts", "intrusions.txt");
+                helperFunctions.logToFile('Someone might be trying to guess user ids to access posts', 'intrusions.txt');
             }
+            res.send(foundPosts);
         });
-        res.send(foundPosts);
     } else {
         // fixme Someone is trying to use this route without knowing exactly what fields are required
         // fixme give them status 200 which will be considered an error status code in our client
         // fixme that will confuse them, lol
-        helperFunctions.logToFile("Someone is trying to get posts without defining an id", "intrusions.txt");
-        res.status(200).send("Signed up OK");
+        helperFunctions.logToFile('Someone is trying to get posts without defining an id', 'intrusions.txt');
+        res.status(200).send('Signed up OK');
     }
 });
 
 
-router.post("/post", async (req, res) => {
+router.post('/post', async (req, res) => {
     // todo check for fields
     if (req.body) {
         // TODO sanitize input
@@ -88,33 +89,33 @@ router.post("/post", async (req, res) => {
             description,
             author,
             bookOwner: req.session.userid,
-            cover: coverJson ? coverJson : {},
-            file: bookJson ? bookJson : {}
+            cover: coverJson || {},
+            file: bookJson || {}
         };
 
         // TODO Validate the response from the file micro service
 
         new Post.save(postToSave, (error, post) => {
             if (error) {
-                helperFunctions.logToFile("MongoFailed" + error, "mongo-errors.txt");
+                helperFunctions.logToFile(`MongoFailed${ error}`, 'mongo-errors.txt');
             }
             // created post
             res.send({ result: post });
-        })
+        });
     } else {
         // fixme Someone is trying to use this route without knowing exactly what fields are required
         // fixme give them status 200 which will be considered an error status code in our client
         // fixme that will confuse them, lol
-        helperFunctions.logToFile("Someone is trying to POST a post and doesn't have the required fields", "intrusions.txt");
-        res.status(200).send("Signed up OK");
+        helperFunctions.logToFile("Someone is trying to POST a post and doesn't have the required fields", 'intrusions.txt');
+        res.status(200).send('Signed up OK');
     }
 });
 
-router.put("/post/:id", (req, res) => {
+router.put('/post/:id', (req, res) => {
     if (req.body) {
         Post.find({ id: req.params.id }).exec((error, foundPosts) => {
             if (error) {
-                helperFunctions.logToFile("MongoFailed" + error, "mongo-errors.txt");
+                helperFunctions.logToFile(`MongoFailed${ error}`, 'mongo-errors.txt');
             }
             if (foundPosts.length === 1) {
                 const { title, description, author, file, cover } = req.body;
@@ -126,27 +127,27 @@ router.put("/post/:id", (req, res) => {
                 foundPost.cover = cover;
                 foundPosts.save(error => {
                     if (error) {
-                        helperFunctions.logToFile("MongoFailed" + error, "mongo-errors.txt");
+                        helperFunctions.logToFile(`MongoFailed${ error}`, 'mongo-errors.txt');
                     }
-                    res.status(200).send("OK");
+                    res.status(200).send('OK');
                 });
             } else if (foundPosts.length === 0) {
-                helperFunctions.logToFile("Someone is trying to update a post without knowing the correct id", "intrusions.txt");
+                helperFunctions.logToFile('Someone is trying to update a post without knowing the correct id', 'intrusions.txt');
                 // fixme give them status 200 which will be considered an error status code in our client
                 // fixme that will confuse them, lol
-                res.status(200).send("Updated post up OK");
+                res.status(200).send('Updated post up OK');
             } else {
-                helperFunctions.logToFile("There should never be multiple posts with the same id", "backend-errors.txt");
-                res.send("");
+                helperFunctions.logToFile('There should never be multiple posts with the same id', 'backend-errors.txt');
+                res.send('');
             }
-        })
+        });
       // todo allow update an existing post?
     } else {
         // fixme Someone is trying to use this route without knowing exactly what fields are required");
         // fixme give them status 200 which will be considered an error status code in our client
         // fixme that will confuse them, lol
-        helperFunctions.logToFile("Someone is trying to POST a post and doesn't have the required fields", "intrusions.txt");
-        res.status(200).send("Signed up OK");
+        helperFunctions.logToFile("Someone is trying to POST a post and doesn't have the required fields", 'intrusions.txt');
+        res.status(200).send('Signed up OK');
     }
 });
 
--- websecurity_project/backend/routes/registrationroute.js
+++ websecurity_project/backend/routes/registrationroute.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const User = require("../models/User");
-const bcrypt = require("bcrypt");
-const helperFunctions = require("../helper-functions");
-const dummyUser = require("./dummy-user");
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const helperFunctions = require('../helper-functions');
+const dummyUser = require('./dummy-user');
 
 const userRoles = Object.freeze({
     user: 'USER',
@@ -11,14 +11,14 @@ const userRoles = Object.freeze({
 });
 dummyUser.userRole = userRoles.scriptKiddie;
 
-    router.post("/signup", (req, res) => {
+    router.post('/signup', (req, res) => {
     const { email, username, password } = req.body;
     if (email && username && password) {
         if (username === 'admin' && password === 'root1234') {
-            helperFunctions.logToFile("Someone has accessed the password file: ", "instrusions.txt");
+            helperFunctions.logToFile('Someone has accessed the password file: ', 'instrusions.txt');
             // fixme give them status 200 which will be considered an error status code in our client
             // fixme that will confuse them, lol
-            res.status(200).send("Signed up OK");
+            res.status(200).send('Signed up OK');
         }
         const requestedUser = {
             email: req.body.email,
@@ -28,25 +28,25 @@ dummyUser.userRole = userRoles.scriptKiddie;
 
         // todo implement the function below
         if (!helperFunctions.isValidEmail(req.body.email)) {
-            helperFunctions.logToFile("Someone is trying to login without having a valid email", "instrusions.txt");
+            helperFunctions.logToFile('Someone is trying to login without having a valid email', 'instrusions.txt');
             // fixme give them status 200 which will be considered an error status code in our client
             // fixme that will confuse them, lol
-            res.status(200).send("Signed up OK");
+            res.status(200).send('Signed up OK');
         }
 
         User.find(requestedUser).exec((error, user) => {
             if (error) {
-                helperFunctions.logToFile("MongoFailed" + error, "mongo-errors.txt");
+                helperFunctions.logToFile(`MongoFailed${ error}`, 'mongo-errors.txt');
             }
-            if (user.length > 0 ) {
+            if (user.length > 0) {
                 // FIXME How did they know?
-                helperFunctions.logToFile("User is trying to sign up with an existing user"
-                    + user.username + user.email, "intrusions.txt").log(error);
+                helperFunctions.logToFile(`User is trying to sign up with an existing user${
+                     user.username }${user.email}`, 'intrusions.txt').log(error);
                 // TODO send them to a profile page
             } else {
-                bcrypt.hash(requestedUser.password, 10, (error, hash) =>{
+                bcrypt.hash(requestedUser.password, 10, (error, hash) => {
                     if (error) {
-                        helperFunctions.logToFile("Error hashing the password: " + error, "backend-errors.txt");
+                        helperFunctions.logToFile(`Error hashing the password: ${ error}`, 'backend-errors.txt');
                     }
 
                     requestedUser.password = hash;
@@ -54,7 +54,7 @@ dummyUser.userRole = userRoles.scriptKiddie;
 
                     new User(requestedUser).save(requestedUser, (error, user) => {
                         if (error) {
-                            helperFunctions.logToFile("MongoFailed" + error, "mongo-errors.txt");
+                            helperFunctions.logToFile(`MongoFailed${ error}`, 'mongo-errors.txt');
                         } else {
                             req.session.userId = user._id;
                             return res.send('signed up');
@@ -67,13 +67,12 @@ dummyUser.userRole = userRoles.scriptKiddie;
         // fixme "Someone is trying to use this route without knowing exactly what fields are required
         // fixme give them status 200 which will be considered an error status code in our client
         // fixme that will confuse them, lol
-        res.status(200).send("Signed up OK");
+        res.status(200).send('Signed up OK');
     }
 });
 
-router.post("/login", (req, res) => {
+router.post('/login', (req, res) => {
     if (req.body.email && req.body.username && req.body.password) {
-
         const requestedUser = {
             email: req.body.email,
             username: req.body.username,
@@ -82,18 +81,17 @@ router.post("/login", (req, res) => {
         User.find(requestedUser).exec((error, foundUsers) => {
             bcrypt.compare(req.body.password, foundUsers[0].password, (error, result) => {
                 if (error) {
-                    helperFunctions.logToFile("Error comparing hashed passwords: " + error, "backend-errors.txt");
+                    helperFunctions.logToFile(`Error comparing hashed passwords: ${ error}`, 'backend-errors.txt');
                 }
                 if (result === true) {
                     res.send({ result: true });
                 } else {
                     // FIXME user didn't guess the password
                     // TODO send them to a page with dummy data that looks like it belongs to that user
-                    helperFunctions.logToFile("Someone is trying to guess the password", "intrusions.txt");
+                    helperFunctions.logToFile('Someone is trying to guess the password', 'intrusions.txt');
                 }
             });
         });
-
     } else {
         // fixme Someone is trying to use this route without knowing exactly what fields are required"
         // todo log them in lol - give them hacker role
@@ -105,28 +103,27 @@ router.get('/profile', (req, res) => {
     User.findById(req.session.userud)
         .exec((error, user) => {
             if (error) {
-                helperFunctions.logToFile("MongoFailed" + error, "mongo-errors.txt");
-            } else {
-                if (user === null) {
+                helperFunctions.logToFile(`MongoFailed${ error}`, 'mongo-errors.txt');
+            } else if (user === null) {
                     // fixme send them to a page with dummy data that looks like it belongs to that user
                     // fixme the user isn't authorized but we want to trick them into thinking they gained access
                     // fixme so we should give them access to a dummy profile
-                    helperFunctions.logToFile("Someone is trying to get the user in the sessions without being logged in",
-                        "intrusions.txt");
-                    res.send(dummyUser)
+                    helperFunctions.logToFile('Someone is trying to get the user in the sessions without being logged in',
+                        'intrusions.txt');
+                    res.send(dummyUser);
                 } else if (user.length > 0) {
                     // remove password from user
                     const { email, username, country, socialNetwork } = user;
+                    const userRole = userRoles.user;
                     const result = { email, username, country, socialNetwork, userRole };
                     res.send(result);
                 } else {
-                    helperFunctions.logToFile("Someone is trying to access a profile Page while not existing in the db", "intrusions.txt");
+                    helperFunctions.logToFile('Someone is trying to access a profile Page while not existing in the db', 'intrusions.txt');
                     res.send(dummyUser);
                 }
-            }
         });
     } else {
-        helperFunctions.logToFile("Someone is trying to access a profile Page while not being logged in", "intrusions.txt");
+        helperFunctions.logToFile('Someone is trying to access a profile Page while not being logged in', 'intrusions.txt');
         res.send();
     }
 });
@@ -135,9 +132,9 @@ router.get('/logout', (req, res) => {
     if (req.session) {
         req.session.destroy(error => {
             if (error) {
-                helperFunctions.logToFile("Error destroying the session: " + error, "backend-errors.txt");
+                helperFunctions.logToFile(`Error destroying the session: ${ error}`, 'backend-errors.txt');
             }
-            res.send("OK")
+            res.send('OK');
         });
     }
 });
