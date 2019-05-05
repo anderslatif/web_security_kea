2038f3efb0fbf212ba2afe3cbc1ec6ecfce78d58
Cosmin Burlacu <cosmin.burlacu@yahoo.com>
2019-05-05 21:13:27 +0200

    register validation

--- websecurity_project/src/reducers/usersReducer.js
+++ websecurity_project/src/reducers/usersReducer.js
@@ -8,44 +8,46 @@ import {
 // import { LOGIN_USER } from "../actions/actionsVariables";
 import { FETCH_USER, REGISTER_USER, LOGIN_USER, UPDATE_POST, EDIT_PROFILE, PROFILE_IMAGE } from "../actions/actionsVariables"; 
 
-const usersReducerDefaultReducer = {
-    id:"userid1",
-    name: "user45",
-    email:"email@email.com",
-    country: "South Korea",
-    socialNetwork: "facebook__link",
-    image: "./image/profile__image.jpg",
-    books: [
-        {
-            bookId: "bookid1",
-            cover: "./image/book__cover1.jpg",
-            file: "fileLocation",
-            title: "title1",
-            author: "author1",
-            reviews: [
-                {
-                    userReviewId: "userReviewId1",
-                    reviewTimestamp: 123456789,
-                    review: "this is just a review1"
-                }
-            ]
-        },
-        {
-            bookId: "bookid2",
-            cover: "./image/book__cover2.jpg",
-            file: "fileLocation",
-            title: "title2",
-            author: "author2",
-            reviews: [
-                {
-                    userReviewId: "userReviewId2",
-                    reviewTimestamp: 123456789,
-                    review: "this is just a review2"
-                }
-            ]
-        }
-    ]
-};
+const usersReducerDefaultReducer = [];
+
+// const usersReducerDefaultReducer = {
+//     id:"userid1",
+//     name: "user45",
+//     email:"email@email.com",
+//     country: "South Korea",
+//     socialNetwork: "facebook__link",
+//     image: "./image/profile__image.jpg",
+//     books: [
+//         {
+//             bookId: "bookid1",
+//             cover: "./image/book__cover1.jpg",
+//             file: "fileLocation",
+//             title: "title1",
+//             author: "author1",
+//             reviews: [
+//                 {
+//                     userReviewId: "userReviewId1",
+//                     reviewTimestamp: 123456789,
+//                     review: "this is just a review1"
+//                 }
+//             ]
+//         },
+//         {
+//             bookId: "bookid2",
+//             cover: "./image/book__cover2.jpg",
+//             file: "fileLocation",
+//             title: "title2",
+//             author: "author2",
+//             reviews: [
+//                 {
+//                     userReviewId: "userReviewId2",
+//                     reviewTimestamp: 123456789,
+//                     review: "this is just a review2"
+//                 }
+//             ]
+//         }
+//     ]
+// };
 
 export default (state = usersReducerDefaultReducer, action) => {
     switch(action.type) {
