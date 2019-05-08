7e263dcecb37a13cc6d80fbceb62a33227e4b85a
anders <anderslatif@gmail.com>
2019-05-08 17:02:21 +0200

    Added https setup for the backend

--- websecurity_project/backend/routes/postroute.js
+++ websecurity_project/backend/routes/postroute.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 const helperFunctions = require('../helper-functions');
-const mongoose = require('mongoose');
 
 router.get('/posts', (req, res) => {
     if (req.session.userId) {
