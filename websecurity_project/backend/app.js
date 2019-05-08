4504cf6da85d0ce4e290d31f74fbfdc228d90198
anders <anderslatif@gmail.com>
2019-05-08 17:23:19 +0200

    Added OPTIONS to cors

--- websecurity_project/backend/app.js
+++ websecurity_project/backend/app.js
@@ -110,7 +110,11 @@ const corsMiddleware = (req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
 
-    next();
+    if (req.method == 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
 };
 
 app.use(corsMiddleware);
