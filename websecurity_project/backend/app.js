3fb4a2134a4a74ad7ef1c6bfe66c0bece5213ef6
anders <anderslatif@gmail.com>
2019-05-08 18:28:53 +0200

    Added CORS logging

--- websecurity_project/backend/app.js
+++ websecurity_project/backend/app.js
@@ -110,7 +110,10 @@ const corsMiddleware = (req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
 
+    console.log("testtesttest")
+
     if (req.method == 'OPTIONS') {
+        console.log("OPTIONS")
         res.sendStatus(200);
     } else {
         next();
