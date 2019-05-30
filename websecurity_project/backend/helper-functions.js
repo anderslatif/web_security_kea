684e646fab8add19f02e39b221afc85bc37888ee
anders <anderslatif@gmail.com>
2019-05-30 19:58:47 +0200

    Fixed missing parameter in isValidEmail

--- websecurity_project/backend/helper-functions.js
+++ websecurity_project/backend/helper-functions.js
@@ -40,7 +40,7 @@ module.exports = {
     }
     return value;
   },
-  isValidEmail: () => {
+  isValidEmail: (email) => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   },
